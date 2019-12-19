62530577178f3c743414b5b048db407a9968cf8c
Ted Tran <TedTran2019@gmail.com>
2019-12-19 10:55:40 -0800

    Completed d15

--- d15/d15.rb
+++ d15/d15.rb
@@ -21,21 +21,65 @@ class RepairDroid
 		4 => [0, 1]
 	}
 
+	attr_reader :oxygen_system_pos
+
 	def initialize(program, size)
 		@program = program
 		@grid = Array.new(size) { Array.new(size, 0) }
+		@visited_grid = Array.new(size) { Array.new(size, 0) }
 		@starting_pos = [size / 2, size / 2]
 		@size = size
+		@oxygen_system_pos = nil
 	end
 
 	def min_steps
 		x, y = @starting_pos
+		@grid[y][x] = '.'
 		fewest_steps(x, y, 0)
+	end
+
+	def display_grid
+		@grid.each do |row|
+			row.each do |ele|
+				print "#{ele} "
+			end
+			print "\n"
+		end
+	end
+
+	# Usage of two queues in this BFS to get number of steps
+	# Starting oxygen doesn't count + takes an extra step to see if next_step is empty
+	def oxygen_fill
+		steps = -2
+		current_step = [@oxygen_system_pos]
+		next_step = []
+
+		until current_step.empty? && next_step.empty?
+			until current_step.empty?
+				y, x = current_step.shift
+				if @grid[y][x] == '.' || ([y, x] == @oxygen_system_pos)
+					@grid[y][x] = 'O'
+					DIRS.values.each do |dir|
+						dy, dx = dir
+						next_step << [y + dy, x + dx]
+					end
+				end
+			end
+
+			system('clear')
+			display_grid
+			sleep(0.05)
+
+			current_step, next_step = next_step, current_step
+			steps += 1
+		end
+
+		steps
 	end
 
 	private
 
-	# What if the droid started on the oxygen system?
+	# Assuming droid starts on empty space
 	def fewest_steps(x, y, steps)
 		return nil if out_of_bounds?(x, y) || previously_visited?(x, y)
 		mark_visited(x, y)
@@ -46,17 +90,21 @@ class RepairDroid
 			dy, dx = DIRS[movement_command]
 			case status_code
 			when 0
+				@grid[y + dy][x + dx] = '#'
 				next
 			when 1
+				@grid[y + dy][x + dx] = '.'
 				pos_min = fewest_steps(x + dx, y + dy, steps + 1)
 				unless pos_min.nil?
 					min_steps = pos_min if min_steps.nil? || (pos_min < min_steps)
 				end
 				@program.run_code([OPP[movement_command]])
 			when 2
+				@grid[y + dy][x + dx] = 'O'
 				pos_min = steps + 1
 				if min_steps.nil? || (pos_min < min_steps)
 					min_steps = pos_min
+					@oxygen_system_pos = [y + dy, x + dx]
 				end
 				@program.run_code([OPP[movement_command]])
 			end
@@ -71,17 +119,20 @@ class RepairDroid
 	end
 
 	def previously_visited?(x, y)
-		@grid[y][x] == 1
+		@visited_grid[y][x] == 1
 	end
 
 	def mark_visited(x, y)
-		@grid[y][x] = 1
+		@visited_grid[y][x] = 1
 	end
 
 	def mark_unvisited(x, y)
-		@grid[y][x] = 0
+		@visited_grid[y][x] = 0
 	end
 end
 
 a = Intcode.new('input.txt', true)
-p RepairDroid.new(a, 500).min_steps
+droid = RepairDroid.new(a, 42)
+droid.min_steps
+droid.display_grid
+p droid.oxygen_fill
