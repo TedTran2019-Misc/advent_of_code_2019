815871e4273e15ee54c104e5aa1e8b00435b61c7
Ted Tran <TedTran2019@gmail.com>
2019-12-26 00:26:25 -0800

    Eh, what I have so far... It's still pretty bad. Need to do the 136 input and the main input

--- d18/d18.rb
+++ d18/d18.rb
@@ -28,11 +28,11 @@ class TritonBeam
 	end
 
 	require 'colorize'
-	def display_maze
+	def display_maze(maze, current)
 		system('clear')
-		@maze.each_with_index do |row, y|
+		maze.each_with_index do |row, y|
 			row.each_with_index do |ele, x|
-				if @current == [y, x]
+				if current == [y, x]
 					print ele.type.colorize(:red)
 				else
 					print ele.type
@@ -64,6 +64,8 @@ class TritonBeam
 			steps += 1
 			queue = new_queue
 		end
+
+		steps
 	end
 
 	private
@@ -71,33 +73,54 @@ class TritonBeam
 	def deep_dup(ele)
 		if ele.is_a? Array
 			arr = []
-			ele.each { |e| arr << e.deep_dup }
+			ele.each { |e| arr << deep_dup(e) }
 			return arr
 		else
 			return ele.dup
 		end
 	end
 
-	# Change this once I'm back into it tomorrow
+	# Hmm, error if starting node isn't valid
+	# Optimized kinda by only dupping maze if it's a valid node
+	# Keeping track of visited and not backtracking unless a new key is found
+	# Maybe a way to remove useless nodes? Need a heuristic.
 	def generate_children(node)
-		return [] if out_of_bounds?(node)
-		tile = @maze[node.y][node.x]
-		return [] if tile == '#' ||
-								 (!@doors[tile].nil? && node.keys[tile.downcase].nil?)
-		node.keys[tile] = true if @keys[tile]
+		curr_tile = node.maze[node.y][node.x]
+		curr_face = curr_tile.type
+		return [] if curr_tile.visited
+		if @keys[curr_face] && node.keys[curr_face].nil?
+			node.keys[curr_face] = true
+			reset_all_to_unvisited(node.maze)
+		end
 		return nil if node.keys.length == @nbr_keys
 
+		curr_tile.mark_visited
 		children = []
 		DIRS.each do |dy, dx|
-			children << Node.new(node.keys.dup, dx + node.x, dy + node.y)
+			x = dx + node.x
+			y = dy + node.y
+			next if out_of_bounds?(x, y)
+			tile = node.maze[y][x]
+			face = tile.type
+			next if tile.visited ||
+							face == '#' ||
+							(!@doors[face].nil? && node.keys[face.downcase].nil?)
+
+			children << Node.new(node.keys.dup, dx + node.x, dy + node.y, deep_dup(node.maze))
 		end
 
 		children
 	end
 
-	def out_of_bounds?(node)
-		node.x < 0 || node.y < 0 ||
-		node.x > @maze[0].length || node.y > @maze.length
+	def reset_all_to_unvisited(maze)
+		maze.each do |row|
+			row.each { |tile| tile.unmark_visited }
+		end
+	end
+
+	def out_of_bounds?(x, y)
+		x < 0 || y < 0 ||
+		x > @maze[0].length || y > @maze.length
 	end
 
 	def create_maze(input)
@@ -130,7 +153,7 @@ class TritonBeam
 end
 
 class Node
-	attr_accessor :keys, :x, :y
+	attr_accessor :keys, :x, :y, :maze
 
 	def initialize(keys, x, y, maze)
 		@keys = keys
@@ -141,13 +164,46 @@ class Node
 end
 
 a = File.read('input.txt').chomp
+
+# 8
 a = '#########
 #b.A.@.a#
 #########'
+
+# 86
 a = '########################
 #f.D.E.e.C.b.A.@.a.B.c.#
 ######################.#
 #d.....................#
 ########################'
+
+# 132
+a = '########################
+#...............b.C.D.f#
+#.######################
+#.....@.a.B.c.d.A.e.F.g#
+########################'
+
+# 136
+a = '#################
+#i.G..c...e..H.p#
+########.########
+#j.A..b...f..D.o#
+########@########
+#k.E..a...g..B.n#
+########.########
+#l.F..d...h..C.m#
+#################'
+
+=begin
+# 81
+a = '########################
+#@..............ac.GI.b#
+###d#e#f################
+###A#B#C################
+###g#h#i################
+########################'
+=end
+
 b = TritonBeam.new(a)
-# p b.shortest_path_to_collect_all_keys
+p b.shortest_path_to_collect_all_keys
